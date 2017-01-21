feffb10100bf33679d56dcb8b8f22e8bc557a7b5
Michael Vilabrera <m.vilabrera@gmail.com>
2017-01-21 14:16:49 -0500

    initial commit

---
+++ MasterDetail/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MasterDetail
+//
+//  Created by Michael Vilabrera on 1/21/17.
+//  Copyright © 2017 Michael Vilabrera. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
